8f57e144f63f8822faf8b44386d55d8734dd5b61
Naleen Dissanayake <naleenituom@gmail.com>
2026-04-23 13:25:27 +0900

    Added string constant helper classes

---
+++ PhotoScout/PhotoScout/Features/Search/Constants/SearchStrings.swift
@@ -0,0 +1,16 @@
+//
+//  SearchStrings.swift
+//  PhotoScout
+//
+//  Created by Naleen Dissanayake on 2026/04/23.
+//
+
+import Foundation
+
+enum SearchStrings {
+    static let screenTitle = "PhotoScout"
+    static let searchPlaceholder = "Search photos"
+    static let enterKeywordMessage = "Enter a keyword to search"
+    static let noResultsMessage = "No results found"
+    static let genericErrorMessage = "Something went wrong. Please try again."
+}
